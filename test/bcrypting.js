e073731121325f64a74f724d1cffb2a552558685
Thompson Galetovic <200conceptz@gmail.com>
2019-05-16 12:03:18 -0400

    switching basic to jwt

---
+++ test/bcrypting.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcryptjs')
+
+// ('dunder', 'Dunder Mifflin', null, 'password'),
+// ('b.deboop', 'Bodeep Deboop', 'Bo', 'bo-password'),
+// ('c.bloggs', 'Charlie Bloggs', 'Charlie', 'charlie-password'),
+// ('s.smith', 'Sam Smith', 'Sam', 'sam-password'),
+// ('lexlor', 'Alex Taylor', 'Lex', 'lex-password'),
+// ('wippy', 'Ping Won In', 'Ping', 'ping-password');
+
+// bcrypt.hash('password', 10).then(hash => console.log({ hash }))
+// bcrypt.hash('bo-password', 10).then(hash => console.log({ hash }))
+// bcrypt.hash('charlie-password', 10).then(hash => console.log({ hash }))
+// bcrypt.hash('sam-password', 10).then(hash => console.log({ hash }))
+// bcrypt.hash('lex-password', 10).then(hash => console.log({ hash }))
+// bcrypt.hash('ping-password', 10).then(hash => console.log({ hash }))
+
+// bcrypt.hash('test-string', 16).then(hash => console.log({ hash }))
+// bcrypt.hash('ping-password', 12).then(hash => console.log({ hash }))
